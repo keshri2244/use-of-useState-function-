d3e6a1ea1fc5391f2d458a5a8865a4dc79389455
Rahul Keshari <rahulkeshri2244@gmail.com>
2026-04-01 20:47:22 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-function Home() {
-  return <h2> Home Page</h2>;
-}
-
-function About() {
-  return <h2>About Us </h2>;
-}
-
-function Contact() {
-  return <h2>Contact us</h2>;
-}
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
-      <div>
-       
-        <nav style={{ display: "flex", gap: "20px", marginBottom: "20px" }}>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-        </nav>
+      <nav style={{ padding: "10px", background: "#eee" }}>
+        <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+        <Link to="/about" style={{ marginRight: "10px" }}>About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
 
-   
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>home </p>
+    </div>
+  );
+}
+
+export default Home;
