32522e65010207a69f616ef6698a7dd8ab25e0cf
Rahul Keshari <rahulkeshri2244@gmail.com>
2026-04-01 20:08:08 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-
-import { useState, useEffect } from "react"
-
-function App() {
-  const [query, setQuery] = useState("")
-  const [results, setResults] = useState([])
-
-  useEffect(() => {
-    if (query === "") return
-
-    fetch(`https://jsonplaceholder.typicode.com/users?name_like=${query}`)
-      .then(res => res.json())
-      .then(data => setResults(data))
-  }, [query])
-
-  return (
-    <div>
-      <input onChange={(e) => setQuery(e.target.value)} />
-
-      {results.map(r => (
-        <p key={r.id}>{r.name}</p>
-      ))}
-    </div>
-  )
+function Home() {
+  return <h2> Home Page</h2>;
 }
 
-export default App
+function About() {
+  return <h2>About Us </h2>;
+}
+
+function Contact() {
+  return <h2>Contact us</h2>;
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+       
+        <nav style={{ display: "flex", gap: "20px", marginBottom: "20px" }}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+   
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
